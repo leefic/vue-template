5b1058cb2dd94643a5dd3de5ba9d16cb1b9acc47
lee <408718358@qq.com>
2022-04-21 23:07:16 +0800

    chore: 添加jest测试

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,4 +1,5 @@
-describe('test',()=>{
-    const sum = (a,b)=>a+b
-    expect(sum(1,2)).toBe(3)
-})
+it("test", () => {
+	const sum = (a, b) => a + b;
+	expect(sum(1, 2)).toBe(3);
+	expect(sum(2, 2)).not.toBe(3);
+});
