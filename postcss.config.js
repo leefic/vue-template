06e3fb5c538c7f82684614a59acdf3ddde1d5dca
daifang.deng <1421219606@qq.com>
2022-04-23 14:55:09 +0800

    fix: 修复postcss引起的启动报错

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,17 @@
-import autoprefixer from 'autoprefixer';
-
 module.exports = {
-	plugins: [autoprefixer],
+	plugins: [
+		// 前缀追加
+		require('autoprefixer')({
+			overrideBrowserslist: [
+				'Android 4.1',
+				'iOS 7.1',
+				'Chrome > 31',
+				'ff > 31',
+				'ie >= 8',
+				'> 1%',
+			],
+			grid: true,
+		}),
+		require('postcss-flexbugs-fixes'),
+	],
 };
